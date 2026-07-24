db7d580ba713aaf332ae8c17cb63f696305cc61b
elnazfazli <elnazfazli75@gmail.com>
2026-07-24 15:04:58 +0330

    Create 02_prepare_data.R

---
+++ scripts/02_prepare_data.R
@@ -0,0 +1,17 @@
+# =====================================================
+# EIRT-NBack-Analysis
+# File: 02_prepare_data.R
+# Author: Elnaz Fazli
+# =====================================================
+
+library(dplyr)
+
+# -----------------------------------------------------
+# Data preprocessing
+# -----------------------------------------------------
+
+# Example:
+# data <- data %>%
+#   filter(!is.na(ID))
+
+# Continue preprocessing here...
