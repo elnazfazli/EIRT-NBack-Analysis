dffce2d75544459521b44c93f2a1207204e0060d
elnazfazli <elnazfazli75@gmail.com>
2026-07-24 15:01:11 +0330

    Update 01_load_data.R

--- scripts/01_load_data.R
+++ scripts/01_load_data.R
@@ -1,1 +1,14 @@
+# =====================================================
+# EIRT-NBack-Analysis
+# File: 01_load_data.R
+# Author: Elnaz Fazli
+# =====================================================
 
+library(haven)
+library(dplyr)
+
+# Load data
+data <- read_sav("data/mydata1.sav")
+
+# Display data structure
+str(data)
