2bb75df847f3ec83e18f65e1db17bd409f6edb99
elnazfazli <elnazfazli75@gmail.com>
2026-07-24 15:36:08 +0330

    Update 02_prepare_data.R

--- scripts/02_prepare_data.R
+++ scripts/02_prepare_data.R
@@ -1,17 +1,15 @@
-# =====================================================
-# EIRT-NBack-Analysis
-# File: 02_prepare_data.R
-# Author: Elnaz Fazli
-# =====================================================
-
 library(dplyr)
 
-# -----------------------------------------------------
-# Data preprocessing
-# -----------------------------------------------------
+if ("nback" %in% names(df))
+  df <- df %>% rename(Nback_total = nback)
 
-# Example:
-# data <- data %>%
-#   filter(!is.na(ID))
+if ("timenback" %in% names(df))
+  df <- df %>% rename(RT_ms = timenback)
 
-# Continue preprocessing here...
+if ("IQ" %in% names(df))
+  df <- df %>% rename(Raven_IQ = IQ)
+
+qcols <- grep("^q\\d+$", names(df), value = TRUE)
+
+if (length(qcols) == 0)
+  stop("No item columns found.")
