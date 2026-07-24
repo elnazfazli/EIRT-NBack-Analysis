3a1029a9e16e9772418fc454b897bf7227a13eb4
elnazfazli <elnazfazli75@gmail.com>
2026-07-24 15:25:37 +0330

    Create 03_descriptive_stats.R

---
+++ scripts/03_descriptive_stats.R
@@ -0,0 +1,14 @@
+# =====================================================
+# EIRT-NBack-Analysis
+# File: 03_descriptive_stats.R
+# Author: Elnaz Fazli
+# =====================================================
+
+library(psych)
+library(dplyr)
+
+# Descriptive statistics
+
+summary(data)
+
+describe(data)
