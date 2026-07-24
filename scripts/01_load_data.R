e44a1e26e254caf1c46d4464b800a0c5e4801c2f
elnazfazli <elnazfazli75@gmail.com>
2026-07-24 20:13:49 +0330

    Update 01_load_data.R

--- scripts/01_load_data.R
+++ scripts/01_load_data.R
@@ -1,8 +1,13 @@
+# ============================================================
+# Project : EIRT-NBack-Analysis
+# Author  : Elnaz Fazli
+# Purpose : Load raw dataset
+# ============================================================
 ibrary(haven)    
 library(dplyr)  
 library(psych)    # describe
 library(TAM)      # مدل EIRT / tam.mml / tam.latreg
 library(car)      # vif
-file_path <- "C:/Users/user/Desktop/mydata1.sav"
+file_path <- "data/mydata1.sav"
 if (!file.exists(file_path)) stop("فایل پیدا نشد.بررسی مسیر 'file_path'")
 df <- haven::read_sav(file_path)
