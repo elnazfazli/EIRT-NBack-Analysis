087bcb75413b65dc908de8e91a8f6230e9057975
Elnaz Fazli <elnazfazli75@gmail.com>
2026-07-24 22:26:02 +0330

    Refactor data loading and preprocessing scripts

--- scripts/scripts/01_load_data.R
+++ scripts/scripts/01_load_data.R
@@ -9,5 +9,5 @@ library(psych)    # describe
 library(TAM)      # EIRT / tam.mml / tam.latreg
 library(car)      # vif
 file_path <- "data/mydata1.sav"
-if (!file.exists(file_path)) stop("فایل پیدا نشد.بررسی مسیر 'file_path'")
+if (!file.exists(file_path)) stop("فایل پیدا نشد .بررسی مسیر 'file_path'")
 df <- haven::read_sav(file_path)
