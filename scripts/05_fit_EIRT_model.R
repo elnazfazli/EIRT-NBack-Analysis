fb6495d5a022e45b019004ff2e0e6523abc39232
Elnaz Fazli <elnazfazli75@gmail.com>
2026-07-25 21:42:50 +0330

    Update 05_fit_EIRT_model.R

--- scripts/05_fit_EIRT_model.R
+++ scripts/05_fit_EIRT_model.R
@@ -1,4 +1,4 @@
-# ---------- 8.اجرای مدل EIRT ----------
+# Fit the Explanatory Item Response Theory (EIRT) model
 mod_eirt <- TAM::tam.mml(resp = resp_mat,irtmodel = "EPCM",Q = Q_matrix,
                          control = list(conv = 0.001,snodes = 500,maxiter = 500,verbose = TRUE))
 
