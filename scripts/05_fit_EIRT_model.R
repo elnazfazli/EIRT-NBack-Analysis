2b5486a8abab2befb15f11c60b782e7690fcd82f
Elnaz Fazli <elnazfazli75@gmail.com>
2026-07-25 20:43:55 +0330

    Create 05_fit_EIRT_model.R

---
+++ scripts/05_fit_EIRT_model.R
@@ -0,0 +1,6 @@
+# ---------- 8.اجرای مدل EIRT ----------
+mod_eirt <- TAM::tam.mml(resp = resp_mat,irtmodel = "EPCM",Q = Q_matrix,
+                         control = list(conv = 0.001,snodes = 500,maxiter = 500,verbose = TRUE))
+
+theta_wle <- tryCatch({ TAM::tam.wle(mod_eirt)$theta },error = function(e) { NULL })
+theta_eap <- tryCatch({ TAM::tam.EAP(mod_eirt)$theta },error = function(e) { NULL })
