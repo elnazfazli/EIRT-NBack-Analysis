63ed4ee301a6d0f371cf09f1d26e8ab732f46561
Elnaz Fazli <elnazfazli75@gmail.com>
2026-07-25 21:41:39 +0330

    Update 04_prepare_EIRT_data.R

--- scripts/04_prepare_EIRT_data.R
+++ scripts/04_prepare_EIRT_data.R
@@ -1,10 +1,10 @@
 resp_mat <- as.matrix(sapply(df[,qcols,drop = FALSE],to_numeric_safe))
 Q_matrix <- matrix(1,nrow = ncol(resp_mat),ncol = 1)
 data_neo <- data_neo %>%
-  mutate(RT_ms_log = log(RT_ms)) %>%      # تبدیل لگاریتمی
-  mutate(RT_ms_z = scale(RT_ms_log))      # استانداردسازی Z-score بعد از لگاریتم
+  mutate(RT_ms_log = log(RT_ms)) %>%     # Log transformation
+  mutate(RT_ms_z = scale(RT_ms_log))     # Z-score standardization after log transformation
 
-# ---------- 7.استانداردسازی و تعامل‌ها ----------
+# Standardize variables and create interaction terms
 z_targets <- intersect(c("NEO_N","NEO_E","NEO_O","NEO_A","NEO_C","Raven_IQ","RT_ms"),names(data_neo))
 data_neo <- data_neo %>% mutate(across(all_of (z_targets),~ as.numeric(scale(.x)),.names = "{.col}_z"))
 for (scale in c("NEO_N","NEO_E","NEO_O","NEO_A","NEO_C")) {
@@ -15,7 +15,7 @@ for (scale in c("NEO_N","NEO_E","NEO_O","NEO_A","NEO_C")) {
   }
 }
 
-# لیست پیش‌بین‌ها
+# Define predictor variables
 neo_z <- paste0(c("NEO_N","NEO_E","NEO_O","NEO_A","NEO_C"),"_z")
 interact_z <- paste0(c("NEO_N","NEO_E","NEO_O","NEO_A","NEO_C"),"_x_Raven")
 main_predictors <- c("Raven_IQ_z","RT_ms_z")
