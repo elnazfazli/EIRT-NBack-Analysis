88ef4378b8b5860f43374d05813ed73f22da3f56
Elnaz Fazli <elnazfazli75@gmail.com>
2026-07-25 14:41:06 +0330

    Delete scripts directory

--- scripts/scripts/05_fit_EIRT_model.R
+++
@@ -1,5 +0,0 @@
-mod_eirt <- TAM::tam.mml(resp = resp_mat,irtmodel = "EPCM",Q = Q_matrix,
-                         control = list(conv = 0.001,snodes = 500,maxiter = 500,verbose = TRUE))
-
-theta_wle <- tryCatch({ TAM::tam.wle(mod_eirt)$theta },error = function(e) { NULL })
-theta_eap <- tryCatch({ TAM::tam.EAP(mod_eirt)$theta },error = function(e) { NULL })
